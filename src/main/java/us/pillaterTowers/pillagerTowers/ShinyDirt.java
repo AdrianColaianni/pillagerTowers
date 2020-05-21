af897ef9d61e712dae240c820fe6f85121537f1f
BuildTools <unconfigured@null.spigotmc.org>
2020-05-21 12:31:30 -0500

    super dirt

---
+++ src/main/java/us/pillaterTowers/pillagerTowers/ShinyDirt.java
@@ -0,0 +1,10 @@
+package us.pillaterTowers.pillagerTowers;
+
+import net.minecraft.block.Block;
+
+public class ShinyDirt extends Block {
+
+	public ShinyDirt(Properties properties) {
+		super(properties);
+	}
+}
